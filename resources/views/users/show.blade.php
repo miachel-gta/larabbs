2563df57d63cadeac25267320fdf47539fa90c79
wufan <457838219@qq.com>
2020-03-29 11:39:39 +0000

    显示个人资料

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -10,11 +10,11 @@
     <div class="card ">
       <img class="card-img-top" src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/600/h/600" alt="{{ $user->name }}">
       <div class="card-body">
-            <h5><strong>个人简介</strong></h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-            <hr>
-            <h5><strong>注册于</strong></h5>
-            <p>January 01 1901</p>
+          <h5><strong>个人简介</strong></h5>
+          <p>{{ $user->introduction }}</p>
+          <hr>
+          <h5><strong>注册于</strong></h5>
+          <p>{{ $user->created_at->diffForHumans() }}</p>
       </div>
     </div>
   </div>
@@ -29,7 +29,11 @@
     {{-- 用户发布的内容 --}}
     <div class="card ">
       <div class="card-body">
-        暂无数据 ~_~
+        <h5><strong>个人简介</strong></h5>
+        <p>{{ $user->introduction }}</p>
+        <hr>
+        <h5><strong>注册于</strong></h5>
+        <p>{{ $user->created_at->diffForHumans() }}</p>
       </div>
     </div>
 
