afb041656cdcb0fdb7c91e9e9cc06518026baaef
wufan <457838219@qq.com>
2020-04-04 10:05:09 +0000

    用户可以上传话题图片

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -7,6 +7,7 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
+use App\Handlers\ImageUploadHandler;
 use Auth;
 
 class TopicsController extends Controller
@@ -16,22 +17,30 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
+
+
 	public function index(Request $request, Topic $topic)
 	{
 		$topics = $topic->withOrder($request->order)->paginate(20);
 		return view('topics.index', compact('topics'));
 	}
 
+
+
     public function show(Topic $topic)
     {
         return view('topics.show', compact('topic'));
     }
+
+
 
 	public function create(Topic $topic)
 	{
         $categories = Category::all();
 		return view('topics.create_and_edit', compact('topic', 'categories'));
 	}
+
+
 
 	public function store(TopicRequest $request, Topic $topic)
 	{
@@ -42,11 +51,15 @@ class TopicsController extends Controller
 		return redirect()->route('topics.show', $topic->id)->with('success', '帖子创建成功!');
 	}
 
+
+
 	public function edit(Topic $topic)
 	{
         $this->authorize('update', $topic);
 		return view('topics.create_and_edit', compact('topic'));
 	}
+
+
 
 	public function update(TopicRequest $request, Topic $topic)
 	{
@@ -56,6 +69,8 @@ class TopicsController extends Controller
 		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
 	}
 
+
+
 	public function destroy(Topic $topic)
 	{
 		$this->authorize('destroy', $topic);
@@ -63,4 +78,28 @@ class TopicsController extends Controller
 
 		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
 	}
+
+
+    public function uploadImage(Request $request, ImageUploadHandler $uploader)
+    {
+        // 初始化返回数据，默认是失败的
+        $data = [
+            'success' => false,
+            'msg' => '上传失败!',
+            'file_path' => ''
+        ];
+
+        // 判断是否有上传文件，并赋值给 $file
+        if($file = $request->upload_file) {
+             // 保存图片到本地
+            $result = $uploader->save($request->upload_file, 'topics', \Auth::id(), 1024);
+            // 图片保存成功的话
+            if($request) {
+                $data['file_path'] = $result['path'];
+                $data['msg'] = '上传成功!';
+                $data['success'] = true;
+            }
+        }
+        return $data;
+    }
 }
