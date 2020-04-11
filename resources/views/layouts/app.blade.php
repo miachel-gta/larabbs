030d72037d9f33d02e844db95b76b28b27a8a542
wufan <457838219@qq.com>
2020-04-11 04:08:02 +0000

    站点权限部署

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,6 +38,10 @@
     @include('layouts._footer')
   </div>
 
+  @if (app()->isLocal())
+    @include('sudosu::user-selector')
+  @endif
+
   <!-- Scripts -->
   <script src="{{ mix('js/app.js') }}"></script>
 
